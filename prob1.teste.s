505ec6bd173c010eea92396826ed4dd37908c994
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2023-05-07 20:21:39 -0300

    doc: improve code comments

--- prob1.teste.s
+++ prob1.teste.s
@@ -16,7 +16,7 @@ beq t0, a0, OK1
 beq zero, zero, T2
 OK1: addi s0, s0, 1 #Teste ok, passou
 #Teste 2
-T2: addi a0, zero, 1
+T2: addi a0, zero, 11
 addi a1, zero, 6
 la a2, vetor
 jal ra, primos
@@ -27,23 +27,34 @@ OK2: addi s0, s0, 1 #Teste ok, passou
 beq zero,zero, FIM
 ##### START MODIFIQUE AQUI START #####
 isPrime:
+# Inicializa o iterador como 2
 addi t1, zero, 2
+#  Se o iterador for igual ao limite, então o número é primo, pois não foi encontrado nenhum outro divisor
 increasePrimeTest: beq t1, a4, truePrime
+    # Pego o resto da divisão para determinar se um númeroi é divisível por outro
     rem t2, a4, t1 
     addi t1, t1, 1
+    # Se o resto da divisão do número pelo iterador for diferente de 0, então t1 não é divisor do número, não ferindo a restrição de um número primo
     bne t2, zero, increasePrimeTest 
     addi a3, zero, 0
     jalr zero, 0(x1)
+#  True prime: retorna que o número é primo
 truePrime: addi a3, zero, 1
     jalr zero, 0(x1)
-primos: 
+primos:
+    #  Inicializa s2 com o valor de a0 = começo do intervalo
 	add s2, a0, zero
-    add s4, zero, zero
-    addi s5, zero, 1
+    # Inicializa s6 com o fim do intervalo
     addi s6, a1, 1
+    #  Contabilizador da quantidade de números primos
+    add s4, zero, zero
+    # Valor 1 para ser utilizado como true
+    addi s5, zero, 1
+    # Iterador para testar os números entre a0 e a1
     add a4, s2, zero
+    #  s7 inicializado com o valor 2 para fazer uma validação, na próxima linha,com o começo do intervalo
 	addi s7, zero, 2
-    # Se o valor incial for 1 -> incrementa o iterador em um
+    # Se o valor incial for 1 -> incrementa o iterador em um, pois um não é primo
     bge s2, s7, primosLoop
     addi a4, a4, 1
 primosLoop: beq a4, s6, end
--- prob2.s
+++ prob2.s
@@ -1,5 +1,5 @@
 .data
-cartao: .word 4 9 1 6 6 4 1 8 5 9 3 6 9 0 8 0
+cartao: .word 4 6 7 9 0 7 1 6 4 5 7 2 8 3 5 6
 ##### START MODIFIQUE AQUI START #####
 
 # Este espaço eh para você definir as suas constantes e vetores auxiliares.
@@ -16,19 +16,23 @@ jal ra, verifica
 beq zero,zero,FIM
 ##### START MODIFIQUE AQUI START #####
 verifica:
+        # Guarda o endereço de retorno que chamou o procedimento "verifica"
         addi sp, sp, -4
 	    sw ra, 0(sp)
         jal ra, multvetores
         lw ra, 0(sp)
 	    addi sp, sp, 4
-        lw s5, 0(a0)
+        # Pega a soma que é feita em mulvetores e tira o módulo por 10
         rem s3, a2, s10
+        # Subtrai 10 do resto da soma por 10, caso a subtração seja 10, é feito o módulo de s4 por 10 para considerar que 10 = 0
         sub s4, s10, s3
         rem s4, s4, s10
+        lw s5, 0(a0)
+        # Se a última posição do vetor for igual a conta feita, o cartão é válido. No caso, s5 sempre será o último número do cartão, pois o procedimento multvetores deixa o vetor na posição correta 
         beq s5, s4, valid
-        addi a1, zero, 0
+        addi a0, zero, 0
         beq zero, zero, endVerifica
-valid: addi a1, zero, 1
+valid: addi a0, zero, 1
         beq zero, zero, endVerifica
 endVerifica: jalr zero, 0(ra) 
 multvetores:
@@ -50,7 +54,9 @@ endMulti: add a2, s4, zero
         jalr zero, 0(ra)
 multiOdd: addi s3, zero 1
         mul s7,s6, s1
+        # ONo caso da múltiplicação ser maior que 10, só vai acontecer se a coluna for par, então é verificado se a multiplicação é > 10, se sim, é subtraído por 9 para normalizar o valor
         blt s7, s10, continue
+        # Ex: 9*2 = 18- 9 = 1 + 8 = 9
          addi s7, s7, -9
         beq zero, zero, continue
 multiEven: addi s3, zero, 0
